1d5972a1c27c820077aab064f54eafdab79177a6
Lynne Daniels <lynne33477@gmail.com>
2017-05-16 22:15:06 -0700

    added moment.js and some dynamic html to automagically handle daylight savings time

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Time Zone Conversion Utility
 
 Phase 1
 
-Create display of 4 main USA time zones.  
+Create display of 4 main USA time zones and UTC.
 Onload, populate all zones with current time in that zone.  
 Synchronize the time in all these zones - make update function  
 Allow user to change the time in any zone  
@@ -13,11 +13,15 @@ Use local storage to remember user's hide/show (collapse/expand) preferences
 
 
 Phase 2 (optional) 
-
+Add toggle between 12 and 24 hour clocks.  Local Storage can remember each choice.
 Add current date and user defined date.  
 Update display to reflect date is or is not during daylight savings time  
 Add all Time Zones in US?  US Territories? entire World?  Space?  
 gets complicated - https://www.timeanddate.com/services/api/time-api.html has an API with free trail, $400/yr after 3 months  
 
 12 US Time Zone areas in use now, more after daylight savings ends, still more if Puerto Rice, Guam etc - begin with main four for now  
+
+Other Features to consider:
+
+Description on mouseover of each time zone, especially UTC.
 
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var mainClock = {
     time: '',
-    setTime: function(){
+    update: function(time, utcOffset){//called on update to any text box sets main clock to user choice
+        
 
     },
-    getTime: function(){
-
+    initialize: function(){
+        this.time = Date.now();
     }
+
 };
 function Clock(name, utcOffset){
     this.name = name;
@@ -22,20 +24,78 @@ function buildClocks(timeZones){
     for (var zone in timeZones){
         var clock = new Clock(zone, timeZones[zone]);
         clocks.push(clock);
-        console.log(zone, timeZones[zone]);
-    }
+      }
     return clocks;
 }
 function updateClocks(clock, newTime){
     //triggers on update to time field
 }
+function initializeClocks(clock, newTime){
+    var timeDisplay = document.getElementById('EDT');
+    //TODO get clue of pattern.  need to set interna first, then sort out display
+    timeDisplay.value = timeStr;
+}
 
 function validateTime(newTime){
     //make user enter a valid time
 }
+function renderNYClock(timeStr, abbr, zone, offset){
+    document.getElementById('ET').value = timeStr;
+    document.getElementById('america-new-york-abbr').innerText = abbr;
+    document.getElementById('america-new-york-zone').innerText = zone;
+    document.getElementById('america-new-york-offset').innerText = offset;
+}
+function renderChicagoClock(timeStr, abbr, zone, offset){
+    document.getElementById('CT').value = timeStr;
+    document.getElementById('america-chicago-abbr').innerText = abbr;
+    document.getElementById('america-chicago-zone').innerText = zone;
+    document.getElementById('america-chicago-offset').innerText = offset;
+}
+function initializeMainClock(time, utcoff){
+    var currentTime = new Date();
+    console.log('hrer',currentTime);
+    var UTCstring = currentTime.getUTCHours() + ':' + currentTime.getUTCMinutes();
+    //DOTO fix formatting
+    return UTCstring;
+}
+/*Below code is from http://momentjs.com/timezone/docs/ Moment.js also provides a hook for the 
+long form time zone name. Because these strings are generally localized, Moment Timezone does 
+not provide any long names for zones.
+To provide long form names, you can override moment.fn.zoneName and use the zz token.*/
+var abbrs = {
+    EST : 'Eastern Standard Time',
+    EDT : 'Eastern Daylight Time',
+    CST : 'Central Standard Time',
+    CDT : 'Central Daylight Time',
+    MST : 'Mountain Standard Time',
+    MDT : 'Mountain Daylight Time',
+    PST : 'Pacific Standard Time',
+    PDT : 'Pacific Daylight Time',
+};
 
+moment.fn.zoneName = function () {
+    var abbr = this.zoneAbbr();
+    return abbrs[abbr] || abbr;
+};
 
 //TESTS
+
+var c = moment.tz(Date.now(), "America/New_York");
+var d = moment.tz(Date.now(), "America/Chicago");
+console.log('momentTest: ', c.format(), d.format());
+
+mainClock.initialize();
+renderNYClock(moment.tz(mainClock.time, "America/New_York").format(),
+moment.tz(mainClock.time, "America/New_York").format('z'),
+moment.tz(mainClock.time, "America/New_York").format('zz'),
+moment.tz(mainClock.time, "America/New_York").format('Z'));
+
+renderChicagoClock(moment.tz(mainClock.time, "America/Chicago").format(),
+moment.tz(mainClock.time, "America/Chicago").format('z'),
+moment.tz(mainClock.time, "America/Chicago").format('zz'),
+moment.tz(mainClock.time, "America/Chicago").format('Z'));
+
+/*
 var Test = {};
 Test.assertEquals = function(actual, expected, testName){
     if (actual === expected){
@@ -55,5 +115,8 @@ Test.assertSimilar = function(actual, expected, testName){
     }
 
 };
-clockTest = [{"name":"EDT","utcOffset":-4},{"name":"CDT","utcOffset":-5},{"name":"MDT","utcOffset":-6},{"name":"PDT","utcOffset":-7}];
-Test.assertSimilar(buildClocks(timeZones),clockTest, 'function buildClocks should build four clocks');
+//clockTest = [{"name":"EDT","utcOffset":-4},{"name":"CDT","utcOffset":-5},{"name":"MDT","utcOffset":-6},{"name":"PDT","utcOffset":-7}];
+//Test.assertSimilar(buildClocks(timeZones),clockTest, 'function buildClocks should build four clocks');
+
+//console.log(formatTime(1,2));
+*/
